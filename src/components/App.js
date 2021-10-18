61b7f3bce4835aca37e4d4b373af48f6f62b1c4e
ghelpo <m.f.ragab5890@gmail.com>
2021-10-18 21:56:53 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,58 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
+import { Route, Redirect } from 'react-router-dom'
+import { handleInitialData } from '../actions/shared'
+import NavBar from './NavBar'
+import Login from './Login'
+import SignUp from './SignUp'
 
 class App extends React.Component {
-  render(){
-    return (
-      <div className="App">
-        Would you Rather!!
-      </div>
-    );
+
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
   }
 
+  render(){
+    const {authedUser} = this.props
+    if (authedUser !== null) {
+      return (
+        <Route exact path='/' render ={() => (
+          <Fragment>
+            <NavBar />
+            <div className="ui segment">
+              <p></p>
+            </div>
+          </Fragment>
+          )}
+        />
+
+      );
+    }
+    else {
+      return (
+        <Fragment>
+          <Route exact path='/sign-up' render ={() => (
+              <SignUp />
+            )}
+          />
+          <Route exact path='/log-in' render ={() => (
+              <Login />
+            )}
+          />
+        <Redirect to='/log-in' />
+        </Fragment>
+
+
+      );
+    }
+  }
 }
 
-export default App;
+const mapStateToProps = ({authedUser}) => {
+  return {
+    authedUser,
+    loading : authedUser === null
+  };
+}
+
+export default connect(mapStateToProps)(App)
