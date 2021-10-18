0b6067b5f7321e89c2829c78596f54bf19a348bd
ghelpo <m.f.ragab5890@gmail.com>
2021-10-18 21:54:12 +0200

    created-reducers

---
+++ src/reducers/questions.js
@@ -0,0 +1,21 @@
+import { RECEIVE_QUESTIONS, ADD_QUESTION_ANSWER, ADD_QUESTION } from '../actions/questions'
+
+const questions = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_QUESTIONS:
+      return {
+        ...state,
+        ...action.questions
+      };
+        case ADD_QUESTION:
+          const {question} = action
+          return {
+            ...state,
+            [question.id] : question,
+          };
+    default:
+      return state;
+  }
+}
+
+export default questions
---
+++ src/reducers/users.js
@@ -0,0 +1,15 @@
+import { RECEIVE_USERS, ADD_USER_TWEET } from '../actions/users'
+
+const users = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return {
+        ...state,
+        ...action.users
+      };
+    default:
+      return state;
+  }
+}
+
+export default users
