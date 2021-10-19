3026ec7dfa57863e802451d64c6c455d153b8064
ghelpo <m.f.ragab5890@gmail.com>
2021-10-19 14:05:27 +0200

    Update users.js
    add user

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,10 +1,18 @@
 //handle users action creator
 
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const ADD_USER = 'ADD_USER'
 
 export const receiveUsers = (users) => {
   return {
     type : RECEIVE_USERS,
     users,
+  };
+}
+
+export const addUser = (user) => {
+  return {
+    type : ADD_USER,
+    user,
   };
 }
