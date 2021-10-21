e07ea9548ae6af6cc89878660b2b0c730f9971ee
ghelpo <m.f.ragab5890@gmail.com>
2021-10-21 15:30:41 +0200

    Update NewQuestion.js
    add UI for adding new question

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -4,6 +4,12 @@ import { Route, Redirect } from 'react-router-dom'
 
 class NewQuestion extends React.Component {
 
+  state = {
+    optionOne: '',
+    optionTwo: '',
+    formComplete: '',
+  }
+
   componentDidMount() {
     console.log(this.props);
   }
@@ -11,15 +17,38 @@ class NewQuestion extends React.Component {
   render(){
     return (
       <div className="ui segment">
-        <p>NewQuestion</p>
+        <h3 className="ui center aligned icon blue header">
+          Create NEW Question!
+          <i className="tiny question circle outline icon"></i>
+        </h3>
+        <hr/>
+        <form className="ui form">
+          <h4 className="ui header">
+            Complete The Question
+          </h4>
+          <h3 className="ui header">
+            Would You Rather.....
+          </h3>
+          <div className="field">
+            <input type="text" name="optionOne" placeholder="Option One" />
+          </div>
+          <h2 className="ui center aligned header">
+            ...OR...
+          </h2>
+          <div className="field">
+            <input type="text" name="optionTwo" placeholder="Option Two" />
+          </div>
+
+          <button className="ui button" type="submit">Submit</button>
+        </form>
       </div>
     );
   }
 }
 
-const mapStateToProps = () => {
+const mapStateToProps = (authedUser) => {
   return {
-
+    authedUser,
   };
 }
 
