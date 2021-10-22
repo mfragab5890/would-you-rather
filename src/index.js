d82d3233e5578b13befffff5a1adf5d0a0cf77ec
ghelpo <m.f.ragab5890@gmail.com>
2021-10-22 22:48:41 +0200

    add-semantic-ui
    used semantic ui for react instead of  using cdn

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import reducer from "./reducers"
 import middleware from './middleware'
 import {BrowserRouter} from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
 
 const store = createStore(reducer, middleware)
 
