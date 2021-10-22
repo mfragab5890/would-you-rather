fb4a9768a44a1bd98fc05502586ab8cee46dd6be
ghelpo <m.f.ragab5890@gmail.com>
2021-10-22 16:13:26 +0200

    Update Question.js
    using with router to push history instead of redirect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, withRouter } from 'react-router-dom'
 import ViewQuestion from './ViewQuestion'
 
 class Question extends React.Component {
@@ -13,48 +13,38 @@ class Question extends React.Component {
   }
   handleViewPoll = async(e) => {
     e.preventDefault()
-    console.log(this.state.viewPoll);
-    await this.setState({
-      viewPoll : true
-    });
+    const { history, qId } = this.props
+    await history.push(`/questions/${qId}`)
   }
 
   render(){
     const { user, answered, qId, answer } = this.props
-    const { viewPoll } = this.state
-    if (viewPoll === true) {
-      return (
-        <Redirect to = {`/questions/${qId}`} />
-      );
-    } else {
-      return (
-        <div className="ui centered teal stacked segment">
-          <div className="ui stackable internally celled grid container">
-            <div className="row">
-              <div className="ten wide column"><h3 className="ui header">{user.name} asks:</h3></div>
+    return (
+      <div className="ui centered teal stacked segment">
+        <div className="ui stackable internally celled grid container">
+          <div className="row">
+            <div className="ten wide column"><h3 className="ui header">{user.name} asks:</h3></div>
+          </div>
+          <div className="row">
+            <div className="four wide column">
+              <img className="ui tiny centered circular image" src={user.avatarURL} alt = 'avatar' />
             </div>
-            <div className="row">
-              <div className="four wide column">
-                <img className="ui tiny centered circular image" src={user.avatarURL} alt = 'avatar' />
-              </div>
-              <div className="nine wide column">
-                <h4 className="ui header">Would you rather!</h4>
-                <h5 className = "ui disabled header">...{answer}...</h5>
-                <button
-                  id = {qId}
-                  className="fluid ui basic labeled icon button"
-                  onClick = {this.handleViewPoll}
-                  >
-                  <i className="eye icon"></i>
-                  View Poll
-                </button>
-              </div>
+            <div className="nine wide column">
+              <h4 className="ui header">Would you rather!</h4>
+              <h5 className = "ui disabled header">...{answer}...</h5>
+              <button
+                id = {qId}
+                className="fluid ui basic labeled icon button"
+                onClick = {this.handleViewPoll}
+                >
+                <i className="eye icon"></i>
+                View Poll
+              </button>
             </div>
           </div>
         </div>
-      );
-    }
-
+      </div>
+    );
   }
 }
 
@@ -78,4 +68,4 @@ const mapStateToProps = ({questions,users, authedUser},{qId,answered}) => {
   };
 }
 
-export default connect(mapStateToProps)(Question)
+export default withRouter(connect(mapStateToProps)(Question))
