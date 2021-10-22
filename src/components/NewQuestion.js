83bbb074c4113f812f20e835845254ae6a7b9e35
ghelpo <m.f.ragab5890@gmail.com>
2021-10-22 16:17:29 +0200

    Update NewQuestion.js
    using withRouter instead of Redirect to push history

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
-import { Route, Redirect } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import {handleAddQuestion} from '../actions/shared'
+
 class NewQuestion extends React.Component {
 
   state = {
     optionOne: '',
     optionTwo: '',
     formComplete: false,
-    toHome : false
   }
 
   componentDidMount() {
@@ -45,12 +45,10 @@ class NewQuestion extends React.Component {
     const optionOneText = this.state.optionOne
     const optionTwoText = this.state.optionTwo
     const author = this.props.authedUser.id
-    const {dispatch} = this.props
+    const {dispatch, history} = this.props
     await dispatch(handleAddQuestion({optionOneText, optionTwoText, author}))
     if (window.confirm("Question submited successfully, go to home page?")) {
-      this.setState({
-        toHome : true,
-      })
+      history.push('/')
     }
     else {
       this.setState({
@@ -63,55 +61,50 @@ class NewQuestion extends React.Component {
 
   render(){
     const {optionOne, optionTwo, formComplete, toHome} = this.state
-    if (toHome === true) {
-      return <Redirect to='/' />;
-    } else {
-      return (
-        <div className="ui segment">
-          <h3 className="ui center aligned icon blue header">
-            Create NEW Question!
-            <i className="tiny question circle outline icon"></i>
+    return (
+      <div className="ui segment">
+        <h3 className="ui center aligned icon blue header">
+          Create NEW Question!
+          <i className="tiny question circle outline icon"></i>
+        </h3>
+        <hr/>
+        <form className="ui form">
+          <h4 className="ui header">
+            Complete The Question
+          </h4>
+          <h3 className="ui header">
+            Would You Rather.....
           </h3>
-          <hr/>
-          <form className="ui form">
-            <h4 className="ui header">
-              Complete The Question
-            </h4>
-            <h3 className="ui header">
-              Would You Rather.....
-            </h3>
-            <div className="field">
-              <input
-              type="text"
-              name="optionOne"
-              placeholder="Option One"
-              value = {optionOne}
-              onChange = {this.handleInputChange}
-              />
-            </div>
-            <h2 className="ui center aligned header">
-              ...OR...
-            </h2>
-            <div className="field">
-              <input
-              type="text"
-              name="optionTwo"
-              placeholder="Option Two"
-              value = {optionTwo}
-              onChange = {this.handleInputChange}
-              />
-            </div>
+          <div className="field">
+            <input
+            type="text"
+            name="optionOne"
+            placeholder="Option One"
+            value = {optionOne}
+            onChange = {this.handleInputChange}
+            />
+          </div>
+          <h2 className="ui center aligned header">
+            ...OR...
+          </h2>
+          <div className="field">
+            <input
+            type="text"
+            name="optionTwo"
+            placeholder="Option Two"
+            value = {optionTwo}
+            onChange = {this.handleInputChange}
+            />
+          </div>
 
-            <button
-            className={formComplete === false ? "ui fluid disabled blue button" : "ui fluid blue button"}
-            type="submit"
-            onClick = {this.handleAddNewQuestion}
-            >Submit</button>
-          </form>
-        </div>
-      );
-    }
-
+          <button
+          className={formComplete === false ? "ui fluid disabled blue button" : "ui fluid blue button"}
+          type="submit"
+          onClick = {this.handleAddNewQuestion}
+          >Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
@@ -121,4 +114,4 @@ const mapStateToProps = ({authedUser}) => {
   };
 }
 
-export default connect(mapStateToProps)(NewQuestion)
+export default withRouter(connect(mapStateToProps)(NewQuestion))
